247cc71108ac6ac272251965a5252741694a2ad2
Philip Griesbacher <philip@griesbacher.org>
2017-06-12 12:31:21 +0200

    added spelling

--- indexer.py
+++ indexer.py
@@ -2,8 +2,7 @@ from elasticsearch import Elasticsearch
 
 from csv_handling import load_tweet_csv
 from filter import get_filter_from_index
-from indices import INDEX_ALL_FILTERED_STEMED, INDEX_ALL_FILTERED_LEMED, INDEX_ALL_FILTERED, INDEX_60k_FILTERED_STEMED, \
-    INDEX_60k_FILTERED_LEMED, INDEX_60k_FILTERED, INDEX_60k_FILTERED_CERTAIN_LEMED, INDEX_ALL_FILTERED_CERTAIN_LEMED
+import indices
 
 TRAININGS_DATA_FILE = "train.csv"
 TEST_DATA_FILE = "test.csv"
@@ -20,35 +19,49 @@ def index_data(tweets, index_name="train"):
 
 
 def index_all_filtered_stemed(tweets):
-    index_data(get_filter_from_index(INDEX_ALL_FILTERED_STEMED)(tweets), INDEX_ALL_FILTERED_STEMED)
+    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_STEMED)(tweets), indices.INDEX_ALL_FILTERED_STEMED)
 
 
 def index_all_filtered_lemed(tweets):
-    index_data(get_filter_from_index(INDEX_ALL_FILTERED_LEMED)(tweets), INDEX_ALL_FILTERED_LEMED)
+    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_LEMED)(tweets), indices.INDEX_ALL_FILTERED_LEMED)
 
 
 def index_all_filtered_certain_lemed(tweets):
-    index_data(get_filter_from_index(INDEX_ALL_FILTERED_CERTAIN_LEMED)(tweets), INDEX_ALL_FILTERED_CERTAIN_LEMED)
+    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_CERTAIN_LEMED)(tweets),
+               indices.INDEX_ALL_FILTERED_CERTAIN_LEMED)
 
 
 def index_all_filtered(tweets):
-    index_data(get_filter_from_index(INDEX_ALL_FILTERED)(tweets), INDEX_ALL_FILTERED)
+    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED)(tweets), indices.INDEX_ALL_FILTERED)
+
+
+def index_all_filtered_spelled_lemed(tweets):
+    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_SPELLED_LEMED)(tweets),
+               indices.INDEX_ALL_FILTERED_SPELLED_LEMED)
 
 
 def index_60k_filtered_stemed(tweets):
-    index_data(get_filter_from_index(INDEX_60k_FILTERED_STEMED)(tweets[:60000]), INDEX_60k_FILTERED_STEMED)
+    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_STEMED)(tweets[:60000]),
+               indices.INDEX_60k_FILTERED_STEMED)
 
 
 def index_60k_filtered_lemed(tweets):
-    index_data(get_filter_from_index(INDEX_60k_FILTERED_LEMED)(tweets[:60000]), INDEX_60k_FILTERED_LEMED)
+    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_LEMED)(tweets[:60000]),
+               indices.INDEX_60k_FILTERED_LEMED)
 
 
 def index_60k_filtered(tweets):
-    index_data(get_filter_from_index(INDEX_60k_FILTERED)(tweets[:60000]), INDEX_60k_FILTERED)
+    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED)(tweets[:60000]), indices.INDEX_60k_FILTERED)
 
 
 def index_60k_filtered_certain_lemed(tweets):
-    index_data(get_filter_from_index(INDEX_60k_FILTERED_CERTAIN_LEMED)(tweets[:60000]), INDEX_60k_FILTERED_CERTAIN_LEMED)
+    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_CERTAIN_LEMED)(tweets[:60000]),
+               indices.INDEX_60k_FILTERED_CERTAIN_LEMED)
+
+
+def index_60k_filtered_spelled_lemed(tweets):
+    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_SPELLED_LEMED)(tweets[:60000]),
+               indices.INDEX_60k_FILTERED_SPELLED_LEMED)
 
 
 if __name__ == '__main__':
