07d4a1b96ba4aff4cef3af61d0b5a90d58b7d121
Philip Griesbacher <philip@griesbacher.org>
2017-06-22 10:45:19 +0200

    speed up merge

--- tools.py
+++ tools.py
@@ -1,3 +1,5 @@
+import time
+
 import indices
 from tweet import Tweet
 import numpy as np
@@ -34,14 +36,12 @@ def create_term_vectors_as_array(index, tweets):
     termvectors = _get_term_vectors_from_es_and_build_voc(index, tweets)
 
     print "merging term vectors"
-    x = None
-    for tweet in tweets:
-        if x is None:
-            x = np.array([np.array(map(lambda x: float(termvectors[tweet.get_id()].get(x, 0)), vocabulary))])
-        else:
-            x = np.append(x,
-                          [np.array(map(lambda x: float(termvectors[tweet.get_id()].get(x, 0)), vocabulary))],
-                          axis=0)
+    start = time.time()
+    x = np.empty((len(tweets), len(vocabulary)))
+    for i in range(len(tweets)):
+        x[i] = np.array(map(lambda v: float(termvectors[tweets[i].get_id()].get(v, 0)), vocabulary))
+    print "merging term vectors took:", time.time() - start
+
     return x
 
 
@@ -53,3 +53,4 @@ def probability_to_percent(prob):
 if __name__ == '__main__':
     tweets = [Tweet({"id": 1}), Tweet({"id": 2})]
     print create_term_vectors_as_dict(indices.INDEX_60k_FILTERED_LEMED, tweets)
+    print create_term_vectors_as_array(indices.INDEX_60k_FILTERED_LEMED, tweets)
