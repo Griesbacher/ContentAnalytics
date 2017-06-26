1f8f751851f1172bcaac07ae65fcea1c394ba8a8
Philip Griesbacher <philip@griesbacher.org>
2017-06-26 14:40:25 +0200

    added reduced mem mode

--- gb.py
+++ gb.py
@@ -5,9 +5,11 @@ from xgboost import XGBClassifier
 
 import indices
 from csv_handling import load_tweet_csv, write_tweets_to_csv
-from tools import  get_binary_feature, Termvectorer
+from tools import get_binary_feature, Termvectorer
 from tweet import Tweet
 import numpy as np
+import sys
+import platform
 
 
 class GB:
@@ -30,7 +32,7 @@ class GB:
                 y, weight = get_binary_feature(tweet[k])
                 ys.append(y)
                 weights.append(weight * 100)
-            self._all_models[k].fit(x, np.array(ys))
+            self._all_models[k].fit(x, np.array(ys), sample_weight=np.array(weights))
 
     def predict_proba(self, tweets):
         term_vectors = self._tv.create_term_vectors_as_dict(self._test_index, tweets)
@@ -40,28 +42,68 @@ class GB:
         for tweet in tweets:
             new_tweet = Tweet().set_id(tweet.get_id())
             for k in Tweet.get_all_keys():
-                # predict = self._all_models[k].predict_proba(np.array([term_vectors[tweet.get_id()]]))
-                predict = self._all_models[k].predict(np.array([term_vectors[tweet.get_id()]]))
-                new_tweet[k] = predict[0]
+                predict = self._all_models[k].predict_proba(np.array([term_vectors[tweet.get_id()]]))
+                new_tweet[k] = predict[0][1]
+                # binary result
+                # predict = self._all_models[k].predict(np.array([term_vectors[tweet.get_id()]]))
+                # new_tweet[k] = predict[0]
             result.append(new_tweet.normalize())
             i += 1
             if i % 1000 == 0:
                 print "GB analysed %d of %d" % (i, len(tweets))
         return result
 
+    def fit_and_predict(self, train_tweets, test_tweets):
+        """reduces the memory a lot"""
+        print "fetching termvectors"
+        start = time.time()
+        x = self._tv.create_term_vectors_as_array(self._train_index, train_tweets)
+        test_term_vectors = self._tv.create_term_vectors_as_dict(self._test_index, test_tweets)
+        print "finished termvectors", time.time() - start
+        result = {tweet.get_id(): Tweet().set_id(tweet.get_id()) for tweet in test_tweets}
+        for k in Tweet.get_all_keys():
+            print "fitting %s" % k
+            ys = []
+            weights = []
+            for tweet in train_tweets:
+                y, weight = get_binary_feature(tweet[k])
+                ys.append(y)
+                weights.append(weight * 100)
+            model = XGBClassifier()
+            model.fit(x, np.array(ys), sample_weight=np.array(weights))
+            print "predicting %s" % k
+            for tweet in test_tweets:
+                predict = model.predict_proba(np.array([test_term_vectors[tweet.get_id()]]))
+                result[tweet.get_id()][k] = predict[0][1]
+        return result.values()
+
 
 if __name__ == '__main__':
+    if platform.system() == "Linux":
+        f = open('gb.log', 'w')
+        sys.stdout = f
+
     all_tweets = load_tweet_csv("train.csv")
     trainings_tweets = all_tweets[:1000]
-    test_tweets = all_tweets[60000:]
-    for index in indices.get_60k_indices():
+    number_of_trainings_tweets = len(trainings_tweets)
+    testing_tweets = all_tweets[60000:]
+    less_mem = True
+    indices_to_test = sys.argv[1:] if len(sys.argv[1:]) > 0 else indices.get_60k_indices()
+    for index in indices_to_test:
+        print "*" * 20 + index + "*" * 20
         gbm = GB(index, index.replace("_60k_", "_all_").replace("_certain", ""))
-        print "starting fit"
-        start = time.time()
-        gbm.fit(trainings_tweets)
-        print "finished fit", time.time() - start
-        print "starting predict"
-        start = time.time()
-        result_tweets = gbm.predict_proba(test_tweets)
-        print "finished fit", time.time() - start
+        if less_mem:
+            result_tweets = gbm.fit_and_predict(trainings_tweets, testing_tweets)
+        else:
+            print "starting fit"
+            start = time.time()
+            gbm.fit(trainings_tweets)
+            print "finished fit", time.time() - start
+            print "starting predict"
+            start = time.time()
+            result_tweets = gbm.predict_proba(testing_tweets)
+            print "finished fit", time.time() - start
         write_tweets_to_csv(result_tweets, index + "_gb_binary_normalized.csv")
+
+    if platform.system() == "Linux":
+        f.close()
