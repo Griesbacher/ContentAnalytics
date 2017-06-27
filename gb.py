48243a4c8da37c2d890033938b32bade6639b842
Philip Griesbacher <philip@griesbacher.org>
2017-06-27 15:49:11 +0200

    added multi class for w and s

--- gb.py
+++ gb.py
@@ -75,7 +75,74 @@ class GB:
             for tweet in test_tweets:
                 predict = model.predict_proba(np.array([test_term_vectors[tweet.get_id()]]))
                 result[tweet.get_id()][k] = predict[0][1]
-        return result.values()
+        return [r.normalize() for r in result.values()]
+
+    def fit_and_predict_multi_class(self, train_tweets, test_tweets):
+        print "fetching termvectors"
+        start = time.time()
+        x = self._tv.create_term_vectors_as_array(self._train_index, train_tweets)
+        test_term_vectors = self._tv.create_term_vectors_as_dict(self._test_index, test_tweets)
+        print "finished termvectors", time.time() - start
+        result = {tweet.get_id(): Tweet().set_id(tweet.get_id()) for tweet in test_tweets}
+
+        print "fitting s"
+        model = XGBClassifier()
+        x_s = x
+        for i in range(len(Tweet.get_s_keys()) - 1):
+            x_s = np.append(x_s, x, axis=0)
+        ys = []
+        weights = []
+        for k in Tweet.get_s_keys():
+            k_index = Tweet.get_s_keys().index(k)
+            for tweet in train_tweets:
+                ys.append(k_index)
+                weights.append(tweet[k] * 100)
+        model.fit(x_s, np.array(ys), sample_weight=np.array(weights))
+
+        for tweet in test_tweets:
+            predict = model.predict_proba(np.array([test_term_vectors[tweet.get_id()]]))
+            i = 0
+            for k in Tweet.get_s_keys():
+                result[tweet.get_id()][k] = predict[0][i]
+                i += 1
+        del x_s
+
+        print "fitting w"
+        model = XGBClassifier()
+        x_w = x
+        for i in range(len(Tweet.get_w_keys()) - 1):
+            x_w = np.append(x_w, x, axis=0)
+        ys = []
+        weights = []
+        for k in Tweet.get_w_keys():
+            k_index = Tweet.get_w_keys().index(k)
+            for tweet in train_tweets:
+                ys.append(k_index)
+                weights.append(tweet[k] * 100)
+        model.fit(x_w, np.array(ys), sample_weight=np.array(weights))
+        for tweet in test_tweets:
+            predict = model.predict_proba(np.array([test_term_vectors[tweet.get_id()]]))
+            i = 0
+            for k in Tweet.get_w_keys():
+                result[tweet.get_id()][k] = predict[0][i]
+                i += 1
+        del x_w
+
+        for k in Tweet.get_k_keys():
+            print "fitting %s" % k
+            ys = []
+            weights = []
+            for tweet in train_tweets:
+                y, weight = get_binary_feature(tweet[k])
+                ys.append(y)
+                weights.append(weight * 100)
+            model = XGBClassifier()
+            model.fit(x, np.array(ys), sample_weight=np.array(weights))
+            print "predicting %s" % k
+            for tweet in test_tweets:
+                predict = model.predict_proba(np.array([test_term_vectors[tweet.get_id()]]))
+                result[tweet.get_id()][k] = predict[0][1]
+        return [r.normalize() for r in result.values()]
 
 
 if __name__ == '__main__':
@@ -94,7 +161,7 @@ if __name__ == '__main__':
         print "*" * 20 + index + "*" * 20
         gbm = GB(index, index.replace("_60k_", "_all_").replace("_certain", ""))
         if less_mem:
-            result_tweets = gbm.fit_and_predict(trainings_tweets, testing_tweets)
+            result_tweets = gbm.fit_and_predict_multi_class(trainings_tweets, testing_tweets)
         else:
             print "starting fit"
             start = time.time()
@@ -104,7 +171,8 @@ if __name__ == '__main__':
             start = time.time()
             result_tweets = gbm.predict_proba(testing_tweets)
             print "finished fit", time.time() - start
-        write_tweets_to_csv(result_tweets, index + "_gb_binary_normalized.csv")
+        write_tweets_to_csv(result_tweets,
+                            index + "_gb_%d_percent_weighted_multi_class_normalized.csv" % number_of_trainings_tweets)
 
     if platform.system() == "Linux":
         f.close()
