11eaab0f36993ee99d0c24b291d6a2f1ace538d8
visualJ <benedikt-ringlein@hotmail.de>
2017-07-02 11:24:53 +0200

    simplified indexer

--- indexer.py
+++ indexer.py
@@ -7,6 +7,14 @@ import indices
 TRAININGS_DATA_FILE = "train.csv"
 TEST_DATA_FILE = "test.csv"
 
+indexers = []
+
+
+def indexer(func):
+    """ Registers a function as an indexer. This can be used as decorator. """
+    indexers.append(func)
+    return func
+
 
 def index_data(tweets, index_name="train"):
     es.indices.delete(index=index_name, ignore=[400, 404])
@@ -18,124 +26,113 @@ def index_data(tweets, index_name="train"):
             print "Indexed %i of %i" % (i, len(tweets))
 
 
+def index_data_filtered(tweets, index, count=None):
+    index_data(get_filter_from_index(index)(tweets[:count]), index)
+
+
+@indexer
 def index_all_filtered_stemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_STEMED)(tweets), indices.INDEX_ALL_FILTERED_STEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_STEMED)
 
 
+@indexer
 def index_all_filtered_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_LEMED)(tweets), indices.INDEX_ALL_FILTERED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_LEMED)
 
 
+@indexer
 def index_all_filtered_certain_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_CERTAIN_LEMED)(tweets),
-               indices.INDEX_ALL_FILTERED_CERTAIN_LEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_CERTAIN_LEMED)
 
 
+@indexer
 def index_all_filtered(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED)(tweets), indices.INDEX_ALL_FILTERED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED)
 
 
+@indexer
 def index_all_filtered_certain(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_CERTAIN)(tweets),
-               indices.INDEX_ALL_FILTERED_CERTAIN)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_CERTAIN)
 
 
+@indexer
 def index_all_filtered_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_SPELLED_LEMED)(tweets),
-               indices.INDEX_ALL_FILTERED_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_SPELLED_LEMED)
 
 
+@indexer
 def index_all_filtered_stopped_spelled(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_STOPPED_SPELLED)(tweets),
-               indices.INDEX_ALL_FILTERED_STOPPED_SPELLED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_STOPPED_SPELLED)
 
 
+@indexer
 def index_all_filtered_stopped_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_STOPPED_SPELLED_LEMED)(tweets),
-               indices.INDEX_ALL_FILTERED_STOPPED_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_STOPPED_SPELLED_LEMED)
 
 
+@indexer
 def index_all_filtered_certain_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_CERTAIN_SPELLED_LEMED)(tweets),
-               indices.INDEX_ALL_FILTERED_CERTAIN_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_CERTAIN_SPELLED_LEMED)
 
 
+@indexer
 def index_all_filtered_certain_stopped_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_ALL_FILTERED_CERTAIN_STOPPED_SPELLED_LEMED)(tweets),
-               indices.INDEX_ALL_FILTERED_CERTAIN_STOPPED_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_ALL_FILTERED_CERTAIN_STOPPED_SPELLED_LEMED)
 
 
+@indexer
 def index_60k_filtered_stemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_STEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_STEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_STEMED, 60000)
 
 
+@indexer
 def index_60k_filtered_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_LEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_LEMED, 60000)
 
 
+@indexer
 def index_60k_filtered(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED)(tweets[:60000]), indices.INDEX_60k_FILTERED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED, 60000)
 
 
+@indexer
 def index_60k_filtered_certain(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_CERTAIN)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_CERTAIN)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_CERTAIN, 60000)
 
 
+@indexer
 def index_60k_filtered_certain_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_CERTAIN_LEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_CERTAIN_LEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_CERTAIN_LEMED, 60000)
 
 
+@indexer
 def index_60k_filtered_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_SPELLED_LEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_SPELLED_LEMED, 60000)
 
 
+@indexer
 def index_60k_filtered_stopped_spelled(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_STOPPED_SPELLED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_STOPPED_SPELLED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_STOPPED_SPELLED, 60000)
 
 
+@indexer
 def index_60k_filtered_stopped_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_STOPPED_SPELLED_LEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_STOPPED_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_STOPPED_SPELLED_LEMED, 60000)
 
 
+@indexer
 def index_60k_filtered_certain_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_CERTAIN_SPELLED_LEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_CERTAIN_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_CERTAIN_SPELLED_LEMED, 60000)
 
 
+@indexer
 def index_60k_filtered_certain_stopped_spelled_lemed(tweets):
-    index_data(get_filter_from_index(indices.INDEX_60k_FILTERED_CERTAIN_STOPPED_SPELLED_LEMED)(tweets[:60000]),
-               indices.INDEX_60k_FILTERED_CERTAIN_STOPPED_SPELLED_LEMED)
+    index_data_filtered(tweets, indices.INDEX_60k_FILTERED_CERTAIN_STOPPED_SPELLED_LEMED, 60000)
 
 
 def index_everything(tweets):
-    index_60k_filtered_stemed(tweets)
-    index_60k_filtered_lemed(tweets)
-    index_60k_filtered(tweets)
-    index_60k_filtered_certain(tweets)
-    index_60k_filtered_certain_lemed(tweets)
-    index_60k_filtered_spelled_lemed(tweets)
-    index_60k_filtered_stopped_spelled(tweets)
-    index_60k_filtered_stopped_spelled_lemed(tweets)
-    index_60k_filtered_certain_spelled_lemed(tweets)
-    index_60k_filtered_certain_stopped_spelled_lemed(tweets)
-
-    index_all_filtered_stemed(tweets)
-    index_all_filtered_lemed(tweets)
-    index_all_filtered_certain_lemed(tweets)
-    index_all_filtered(tweets)
-    index_all_filtered_certain(tweets)
-    index_all_filtered_spelled_lemed(tweets)
-    index_all_filtered_stopped_spelled(tweets)
-    index_all_filtered_stopped_spelled_lemed(tweets)
-    index_all_filtered_certain_spelled_lemed(tweets)
-    index_all_filtered_certain_stopped_spelled_lemed(tweets)
+    for index in indexers:
+        index(tweets)
 
 
 if __name__ == '__main__':
