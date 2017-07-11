766681d76aa3c997d24b408f7b3160e67582cfa9
Benedikt Ringlein <benedikt-ringlein@hotmail.de>
2017-07-11 13:09:38 +0200

    fixed get tweet from es in tweet

--- tweet.py
+++ tweet.py
@@ -120,7 +120,7 @@ class Tweet(dict):
             es = Tweet._es
 
         answer = es.get(index=index, id=self.get_id(), doc_type='tweet')
-        return answer['tweet']
+        return answer['_source']['tweet']
 
     @staticmethod
     def get_k_keys():
